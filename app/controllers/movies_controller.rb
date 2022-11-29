1c1048a6f86f25289e108cbd164d0bb8c12b56d1
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-11-29 21:49:51 +0300

    adds params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,5 +16,11 @@ class MoviesController < ApplicationController
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
   end
+  def create
+    movie = Movie.create!(movie_params)
+    render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
   
 end
